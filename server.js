5994e895d663efb393c500c5f5ff41bd5c735787
Tuan Tran <tuan.tran1122@gmail.com>
2016-12-12 19:07:28 -0600

    Heroku addon

--- server.js
+++ server.js
@@ -31,9 +31,9 @@ app.use(express.static("public"));
 var databaseUri = "mongodb://localhost/week18homework";
 
 if (process.env.MONGODB_URI){
-  mongoose.connection(process.env.MONGODB_URI);
+  mongoose.connect(process.env.MONGODB_URI);
 }else{
-  mongoose.connection(databaseUri)
+  mongoose.connect(databaseUri)
 }
 
 var db = mongoose.connection;
